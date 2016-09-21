e0fd3eebdb5c7b429c59020ebe2b5c66513e62d5
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-09-21 16:41:37 -0400

    Add seed data. Add has_many votes to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+  has_many :votes, as: :voteable
 
   validates :content, :commentable_id, :commentable_type, :user_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,39 @@
 20.times do
-  user = User.create!( username: Faker::Internet.user_name,
-               email: Faker::Internet.email,
-               password: "password")
+  user = User.create!(
+    username: Faker::Internet.user_name,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password")
 end
 
 20.times do
-  question = Question.create!( question: Faker::Hipster.sentence,
-               user_id: rand(1..20))
+  question = Question.create!(
+    content: Faker::Hipster.sentence,
+    title: Faker::Name.title,
+    user_id: rand(1..20))
 end
 
 20.times do
-  answer = Answer.create!( answer: Faker::ChuckNorris.fact,
-               question_id: rand(1..20),
-               user_id: rand(1..20))
+  answer = Answer.create!(
+    content: Faker::Hipster.sentence,
+    question_id: rand(1..20),
+    user_id: rand(1..20))
+end
+
+20.times do
+  comment = Comment.create!(
+    content: Faker::Hipster.sentence,
+    question_id: rand(1..20),
+    commentable_id: rand(1..20),
+    commentable_type: ["Answer", "Question"].sample,
+    user_id: rand(1..20))
 end
 
 
-u= User.new(first_name: "boe", last_name: "jenkins", username: "yaBoi", email: "jaBoi@gmail.com", password: "password")
+u= User.create(first_name: "boe", last_name: "jenkins", username: "yaBoi", email: "jaBoi@gmail.com", password: "password")
 
-bob= User.new(first_name: "bob", last_name: "billy", username: "billybob", email: "bob@gmail.com", password: "password")
+bob= User.create(first_name: "bob", last_name: "billy", username: "billybob", email: "bob@gmail.com", password: "password")
 
 q= Question.create(title: "Fake question", content: "testing 123", user_id: 1)
 
