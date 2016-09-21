836c3f6bfd0ed371d6dbfc4119a838125675f640
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-09-21 15:47:43 -0400

    adds models and associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,9 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
+  belongs_to :user
+  has_many :answers
+
+  validates :user_id, :title, :content, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,7 @@
 class Vote < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :voteable, polymorphic: true
+  belongs_to :user
+
+  validates :value, :user_id, :voteable_id, :voteable_type, presence: true
 end
