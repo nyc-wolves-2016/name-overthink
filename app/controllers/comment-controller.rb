eab3590813c4f03c3091a354b5b03695244f610a
Neel Gupta <neelgupta2@gmail.com>
2016-09-22 12:34:50 -0400

    Work on comment controller, problem with throwing errors on questions/show.erb

--- app/controllers/comment-controller.rb
+++ app/controllers/comment-controller.rb
@@ -6,8 +6,25 @@ post '/questions/:question_id/comments/new' do
 
     redirect "/questions/#{@question.id}"
   else
-    @errors = @question_comment.errors.full_messages
+    @question_errors = @question_comment.errors.full_messages
     @question.comments.delete(@question_comment)
+    erb :'questions/show'
+  end
+
+end
+
+
+post '/questions/:question_id/answer/:answer_id/new' do
+  @question = Question.find(params[:question_id])
+  answer = Answer.find(params[:answer_id])
+  @answer_comment = answer.comments.new(content: params[:content], user_id: current_user.id)
+
+  if @answer_comment.save
+
+    redirect "/questions/#{params[:question_id]}"
+  else
+    @answer_errors = @answer_comment.errors.full_messages
+    answer.comments.delete(@answer_comment)
     erb :'questions/show'
   end
 
