79017d9c6f1039601a4e2bd020e31b55d0bdcafa
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-09-22 20:43:07 -0400

    Removes old error messages upon new click

--- public/js/application.js
+++ public/js/application.js
@@ -15,14 +15,15 @@ $(document).ready(function() {
   $('#question-comments').on('submit', '.thumbs_button', function(e) {
     e.preventDefault();
     var data = {comment_id: $(this).children('input').attr('value'), value: $(this).children('button').attr('value')};
+    $("#question-comments").find('p').remove();
     
     $.ajax({
       url: "/votes/new",
       method: "POST",
       data: data
     }).done(function(response) {
-        // debugger;
         if (response.error) {
+            debugger;
             $("input[value='" + response.comment_id + "']").closest('li').append('<p>You already voted on that comment silly-pie</p>')
         } else {
         $("input[value='" + response.comment_id + "']").closest('li').children('span').text(response.total_votes);
