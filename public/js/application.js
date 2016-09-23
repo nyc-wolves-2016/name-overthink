6f68a3ff13435ccb950d8019f9d66ef4f7e497eb
Kimberly Suazo  & Soren Zeliger <pair+kimberlysuazo.soreasy@devbootcamp.com>
2016-09-22 20:35:24 -0400

    Successfully ajaxify the comment votes with error message too

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-
-
-
   $('.questionsList').on('submit', '.thumbs_button', function(e) {
     e.preventDefault();
     var data = {question_id: $(this).children('input').attr('value'), value: $(this).children('button').attr('value')};
@@ -12,6 +9,24 @@ $(document).ready(function() {
       data: data
     }).done(function(response) {
         $("input[value='" + response.question_id + "']").closest('li').children('span').text(response.total_votes);
+    })
+  });
+
+  $('#question-comments').on('submit', '.thumbs_button', function(e) {
+    e.preventDefault();
+    var data = {comment_id: $(this).children('input').attr('value'), value: $(this).children('button').attr('value')};
+    
+    $.ajax({
+      url: "/votes/new",
+      method: "POST",
+      data: data
+    }).done(function(response) {
+        // debugger;
+        if (response.error) {
+            $("input[value='" + response.comment_id + "']").closest('li').append('<p>You already voted on that comment silly-pie</p>')
+        } else {
+        $("input[value='" + response.comment_id + "']").closest('li').children('span').text(response.total_votes);
+        }
     })
   });
 });
